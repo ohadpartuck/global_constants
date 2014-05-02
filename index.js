1b55f652d2b0c2815abd96ffd71acafe0dfe7b5c
ohad partuck <ohadpartuck@gmail.com>
2014-05-02 13:01:13 +0300

    adding more ocnstatns

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 module.exports = {
     sanger: {
         sanger_constants: {
-            'product_data': 'product_data'
+            'product_data': 'product_data',
+            'product_fields': {
+                'id'    : 'id',
+                'name'  : 'name',
+                'stores_available_in' : 'stores_available_in',
+                'tags'  : 'tags',
+                'locale': 'locale'
+            }
         }
     }
 };
