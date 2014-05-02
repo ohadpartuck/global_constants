699fea2cd9f8108cc186dd24613be78bb5828a9e
ohad partuck <ohadpartuck@gmail.com>
2014-05-02 10:13:02 +0300

    writing test

--- test/index.js
+++ test/index.js
@@ -1,13 +1,14 @@
 var should              = require('chai').should(),
     global_constants    = require('../index'),
-    sanger              = global_constants.sanger;
+    sanger              = global_constants['sanger'];
 
 describe('sanger', function() {
-    it('contains global constants', function() {
-        sanger['global_constant'].should.equal({});
+    it('contains constants', function() {
+        sanger['sanger_constants'].should.not.equal({});
+
     });
 
-    it('global constants is an object', function() {
-        sanger['global_constant'].should.equal({});
+    it('constants are a type of an object', function() {
+        sanger['sanger_constants'].should.be.an.instanceOf(Object);
     });
 });
