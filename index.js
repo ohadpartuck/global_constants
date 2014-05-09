200df9346f841cf7ad65d0d8055877c2aa841533
ohad partuck <ohadpartuck@gmail.com>
2014-05-09 17:05:10 +0300

    adding constants

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ module.exports = {
         'bson_id'                   : 'bson_id',
         'search_type_field'         : 'search_type',
         'start_with_search_type'    : 'start_with_search',
+        'starts_with_field'         : 'starts_with_field',
         'starts_with'               : 'starts_with',
-        'max_expansions'            : 'max_expansions'
+        'max_expansions'            : 'max_expansions',
+
+        'regex_search_type'         : 'regex_search',
+        'regex_field'               : 'regex_field',
+        'regex_term'                : 'regex_term'
     },
     sanger: {
         sanger_constants: {
